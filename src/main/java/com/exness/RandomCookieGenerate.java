30376cda4f9ae7e0d0127f3700abf43d1d06b65f
dv1786 <deyankov.vladimir@gmail.com>
2018-10-27 13:13:30 +0600

    added class RandomCookeeGenerate and others

---
+++ src/main/java/com/exness/RandomCookieGenerate.java
@@ -0,0 +1,8 @@
+package com.exness;
+
+public class RandomCookieGenerate {
+   static String cookie = "GA1.2.1336191250.15393"+String.valueOf(Math.random()+100)+"461; ss3=0pl9l5d1fxawnhw3tw53hb1cjqc8zr5v; _gcl_au=1.1.525612295.1539191462; visid_incap_1784725=Koe/dTkHQNyqdccnzcrVjKQyvlsAAAAAQUIPAAAAAADDmc/VkuxqeKfT1BrJ9pNA; visid_incap_1251658=j0Ar2y4BTMqSuy+BD8WlDaQyvlsAAAAAQUIPAAAAAAB8ZX5ixcK+vqzXKSiCDe87; _ym_uid=1539191462968783246; _ym_d=1539191462; __zlcmid=oohhpdcBuKZZmo; cookies_notice_closed=true; regdate=1539191810122; Hm_lvt_3327e6ea6a751efcc3584f7e48c05531=1539191855; exness_user_id=904778; exness_csrfcookie=TlN7ToFv8eUIh8PELpI7GJS4BgVflRdr; visid_incap_961876=y8yDKnXKQaC9q3R+w9jBhKMyvlsAAAAAQkIPAAAAAACAYL6HAbJKSf0tqWjCp57AkkkjkJ/aA4mj; _gid=GA1.2.2059235232.1540570177; _ym_isad=1; nlbi_961876=zDmVOu/by0v9wKoV5LU8xAAAAADt8jEsZ5hvFr2RrtXarMuM; incap_ses_533_961876=iVfLe2l+DBgvMOD3h5llB6nd01sAAAAAO6vO45BNeHcpnPoSB3YtQQ==; nlbi_1251658=whCfYRXENm9+tMIq2I5c+AAAAADYyn1GVPlbY6m1nMYea9J8; _fbp=fb.1.1540611502210.2099756197; fp=e63263eb36c0c8395dec0b5eb92788af; incap_ses_533_1784725=eFaTDjTmH3tNsO/3h5llB5Lr01sAAAAACnaI23q4j3uOtX3w0vnk4A==; incap_ses_533_1251658=HWEsORvXu2ROsO/3h5llB5Lr01sAAAAA5G75bMS6S3xN6rcmYkz7Xw==; language=ru; incap_ses_471_1251658=tzmcROS9cwvtD2o/JFWJBsn501sAAAAA/yuUsN2h81fWEi6hdgwxeQ==; incap_ses_471_961876=w3K0KZTKaSY+vHQ/JFWJBn8D1FsAAAAAoi7Q1UCEGODrv3ZDAITmfQ==; _gat_UA-8651572-1=1; _dc_gtm_UA-8651572-1=1";
+
+
+
+}
