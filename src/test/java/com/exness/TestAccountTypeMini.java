1937b5fa6f5560261c74b3ce27e606ac7c72759a
dv1786 <deyankov.vladimir@gmail.com>
2018-10-28 17:16:48 +0600

    ended tests in Account Type Mini, without check response json

--- src/test/java/com/exness/TestAccountTypeMini.java
+++ src/test/java/com/exness/TestAccountTypeMini.java
@@ -40,7 +40,7 @@ public class TestAccountTypeMini {
                 .queryParam("form_type","mini")
                 .queryParam("instrument","Forex")
                 .queryParam("symbol","AUDCADm")
-                .queryParam("lot","0.1")
+                .queryParam("lot","0.01")
                 .queryParam("leverage","200")
                 .queryParam("user_currency","AUD")
                 .when().get()
@@ -50,7 +50,7 @@ public class TestAccountTypeMini {
                 .body("$", hasKey("margin"))
                 .body("$", hasKey("profit"))
                 .body("user_currency", equalTo("AUD"))
-                .body("profit_formula2", equalTo("0.00010 x 100000.0 x 0.10 = 1.00 CAD"))
+                .body("profit_formula2", equalTo("0.00010 x 100000.0 x 0.01 = 0.10 CAD"))
                 .body("form_type", equalTo("mini"))
                 .extract().response()
                 .prettyPrint();
@@ -180,11 +180,51 @@ public class TestAccountTypeMini {
 
     }
 
-
-//Negative Tests
+    @Test
+    public void lotEnotationValue(){
+        //{"form_type":["Поле обязательно для заполнения."],"lot":["Поле обязательно для заполнения."],"leverage":["Поле обязательно для заполнения."],
+        // "instrument":["Поле обязательно для заполнения."],"symbol":["Поле обязательно для заполнения."],"user_currency":["Поле обязательно для заполнения."]}
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","10e6")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+    }
 
     @Test
-    public void LeverageLargerThanBorder(){
+    public void lotLargerThanBorder(){
+        //{"form_type":["Поле обязательно для заполнения."],"lot":["Поле обязательно для заполнения."],"leverage":["Поле обязательно для заполнения."],
+        // "instrument":["Поле обязательно для заполнения."],"symbol":["Поле обязательно для заполнения."],"user_currency":["Поле обязательно для заполнения."]}
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","3000000")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+
+
+    @Test
+    public void leverageLargerThanBorder(){
         RestAssured.given()
                 .header("cookie",RandomCookieGenerate.cookie)
                 .queryParam("form_type","mini")
@@ -208,7 +248,7 @@ public class TestAccountTypeMini {
     }
 
     @Test
-    public void LeverageLessThanBorder(){
+    public void leverageLessThanBorder(){
         RestAssured.given()
                 .header("cookie",RandomCookieGenerate.cookie)
                 .queryParam("form_type","mini")
@@ -232,7 +272,7 @@ public class TestAccountTypeMini {
     }
 
     @Test
-    public void LeverageIncorrectValue(){
+    public void leverageIncorrectValue(){
         RestAssured.given()
                 .header("cookie",RandomCookieGenerate.cookie)
                 .queryParam("form_type","mini")
@@ -267,6 +307,238 @@ public class TestAccountTypeMini {
                 .queryParam("lot","0.1")
                 .queryParam("leverage","200")
                 .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+    @Test
+    public void queryWithoutInstrumentValue(){
+        //{"form_type":["Поле обязательно для заполнения."],"lot":["Поле обязательно для заполнения."],"leverage":["Поле обязательно для заполнения."],
+        // "instrument":["Поле обязательно для заполнения."],"symbol":["Поле обязательно для заполнения."],"user_currency":["Поле обязательно для заполнения."]}
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","0.1")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+    @Test
+    public void queryWithoutSymbolValue(){
+        //{"form_type":["Поле обязательно для заполнения."],"lot":["Поле обязательно для заполнения."],"leverage":["Поле обязательно для заполнения."],
+        // "instrument":["Поле обязательно для заполнения."],"symbol":["Поле обязательно для заполнения."],"user_currency":["Поле обязательно для заполнения."]}
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","")
+                .queryParam("lot","0.1")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+    @Test
+    public void queryWithoutLotValue(){
+        //{"form_type":["Поле обязательно для заполнения."],"lot":["Поле обязательно для заполнения."],"leverage":["Поле обязательно для заполнения."],
+        // "instrument":["Поле обязательно для заполнения."],"symbol":["Поле обязательно для заполнения."],"user_currency":["Поле обязательно для заполнения."]}
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+    @Test
+    public void queryWithoutLeverageValue(){
+        //{"form_type":["Поле обязательно для заполнения."],"lot":["Поле обязательно для заполнения."],"leverage":["Поле обязательно для заполнения."],
+        // "instrument":["Поле обязательно для заполнения."],"symbol":["Поле обязательно для заполнения."],"user_currency":["Поле обязательно для заполнения."]}
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","0.1")
+                .queryParam("leverage","")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+    @Test
+    public void queryWithoutUserCurrencyValue(){
+        //{"form_type":["Поле обязательно для заполнения."],"lot":["Поле обязательно для заполнения."],"leverage":["Поле обязательно для заполнения."],
+        // "instrument":["Поле обязательно для заполнения."],"symbol":["Поле обязательно для заполнения."],"user_currency":["Поле обязательно для заполнения."]}
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","0.1")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+
+    @Test
+    // Валюта счета в цифровом виде ISO 4217 , EUR = 978
+    public void userCurrencyOnISOValue(){
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","3")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","978")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+    @Test
+    public void userCurrencyOnLowerCaseValue(){
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","3")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","eur")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+    @Test
+    //EURO
+    public void userCurrencyFullNameValue(){
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","3")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","EURO")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+
+
+    @Test
+    public void lotLessThanBorder(){
+//        "lot": [
+//        "Убедитесь что значение больше или равно 0.01."
+//    ]
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","0.0001")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+
+    }
+
+
+    @Test
+    public void lotIncorrectValue(){
+//        "lot": [
+//        "Убедитесь что значение больше или равно 0.01."
+//    ]
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADm")
+                .queryParam("lot","-1")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","AUD")
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("form_type", equalTo("mini"))
+                .extract().response()
+                .prettyPrint();
+    }
+
+
+    @Test
+    //symbolDoesNotMatchAccountType ?
+    public void symbolDoesNotMatchFormType(){
+        RestAssured.given()
+                .header("cookie",RandomCookieGenerate.cookie)
+                .queryParam("form_type","mini")
+                .queryParam("instrument","Forex")
+                .queryParam("symbol","AUDCADc")
+                .queryParam("lot","3")
+                .queryParam("leverage","200")
+                .queryParam("user_currency","EUR")
                 .when().get()
                 .then()
                 .statusCode(200)
